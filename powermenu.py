db8d1e381eff8d271868768a5452ebc3aa1792fb
Shivam_singh <shivamsingh8461@outlook.com>
2022-05-01 13:53:02 +0530

    added new files

---
+++ powermenu.py
@@ -0,0 +1,14 @@
+import os
+
+  
+shutdown = input("Do you wish to power comandlog your computer ? shutdown / restart / log : ")
+  
+if shutdown == 'no':
+    exit()
+elif shutdown=='shutdown':
+    os.system("shutdown /s /t 1")
+elif shutdown=='restart':
+    os.system("shutdown /r /t 1")
+elif shutdown=='log':
+    os.system("shutdown -l")
+
